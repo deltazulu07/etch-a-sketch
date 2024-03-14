1e3eedcbd60a6928b1ef7fdcb9a29ea20e9724e7
JCO <133428143+deltazulu07@users.noreply.github.com>
2024-03-14 15:18:43 -0700

    Draw a 16x16 grid

--- jsEtch.js
+++ jsEtch.js
@@ -1,4 +1,4 @@
-const canvasLength = 3;
+const canvasLength = 16;
 
 function drawDivs(numPerRow) {
     // function draws divis inside parent container
@@ -6,52 +6,43 @@ function drawDivs(numPerRow) {
     // use querySelector to select the parent div 
     const parentDiv = document.querySelector('.divContainer');
 
-    // create a new div element
-    const squareDiv = document.createElement('div');
+    for (let i = 1; i <= numPerRow * numPerRow; i++) {
+        // create a new div element
+        const squareDiv = document.createElement('div');
 
-    //add static CSS styles
-    squareDiv.classList.add('square');
+        //add static CSS styles
+        squareDiv.classList.add('square');
 
-    //use function to dynamically get the correct width for square
-    squareDiv.style.borderWidth = squareWidth(numPerRow);
-
-    // append the new div to the parent div
-    parentDiv.appendChild(squareDiv);
-
-}
-
-function squareWidth(numPerRow) {
-    // function dynamically update square width
-
-    // get the width and border with of the parent div
-    const parent = document.querySelector('.divContainer');
-    // get all computed styles for the parent div
-    const stylesParent = window.getComputedStyle(parent);
-
-    // get the specific properties of parent div
-    const borderWidth = stylesParent.borderWidth;
-    const elementWidth = stylesParent.width;
-
-    // get the square div's border width
-    const squareDiv = document.querySelector('.square');
-    const stylesSquare = window.getComputedStyle(squareDiv);
-    const squareBorderWidth = stylesSquare.borderWidth;
-
-    // compute square width
-    const widthOfsquare = (elementWidth - borderWidth * 2) / numPerRow - squareBorderWidth * 2;
-
-    return widthOfsquare;
+        // append the new div to the parent div
+        parentDiv.appendChild(squareDiv);
+    }
 
 }
+
+// function squareWidth(numPerRow) {
+//     // function dynamically update square width
+
+//     // get the width and border with of the parent div
+//     const parent = document.querySelector('.divContainer');
+//     // get all computed styles for the parent div
+//     const stylesParent = window.getComputedStyle(parent);
+
+//     // get the specific properties of parent div
+//     const borderWidth = stylesParent.borderWidth;
+//     const elementWidth = stylesParent.width;
+
+//     // get the square div's border width
+//     const squareDiv = document.querySelector('.square');
+//     const stylesSquare = window.getComputedStyle(squareDiv);
+//     const squareBorderWidth = stylesSquare.borderWidth;
+
+//     // compute square width
+//     const widthOfsquare = (elementWidth - borderWidth * 2) / numPerRow - squareBorderWidth * 2;
+
+//     return widthOfsquare;
+
+// }
 
 // squareWidth(canvasLength);
 
-drawDivs(canvasLength);
-drawDivs(canvasLength);
-drawDivs(canvasLength);
-drawDivs(canvasLength);
-drawDivs(canvasLength);
-drawDivs(canvasLength);
-drawDivs(canvasLength);
-drawDivs(canvasLength);
 drawDivs(canvasLength);
